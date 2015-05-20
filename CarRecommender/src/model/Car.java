1e0a3f705479460acf774d4575b4abb42c1bdad3
kadiray karakaya <kadiray.k@hotmail.com>
2015-05-20 19:02:08 +0300

    create Car class

---
+++ CarRecommender/src/model/Car.java
@@ -0,0 +1,78 @@
+package model;
+
+public class Car {
+	
+	private String title;
+	private String brand;
+	private String model;
+	private String modelDetail;
+	private int year;
+	private int km;
+	private String color;
+	private int price;
+	private String city;
+	private String town;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}	
+	public String getModelDetail() {
+		return modelDetail;
+	}
+	public void setModelDetail(String modelDetail) {
+		this.modelDetail = modelDetail;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getTown() {
+		return town;
+	}
+	public void setTown(String town) {
+		this.town = town;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getKm() {
+		return km;
+	}
+	public void setKm(int km) {
+		this.km = km;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+
+}

4d2131fe2c4b8d63ac89c41885611f76add92cfb
kadiray karakaya <kadiray.k@hotmail.com>
2015-05-23 15:51:22 +0300

    create IdealCar class

---
+++ CarRecommender/src/model/IdealCar.java
@@ -0,0 +1,38 @@
+package model;
+
+public class IdealCar extends Car{
+
+	private int km_max;
+	private int km_min;
+	private int price_max;
+	private int price_min;
+	
+	public int getKm_max() {
+		return km_max;
+	}
+	public void setKm_max(int km_max) {
+		this.km_max = km_max;
+	}
+	public int getKm_min() {
+		return km_min;
+	}
+	public void setKm_min(int km_min) {
+		this.km_min = km_min;
+	}
+	public int getPrice_max() {
+		return price_max;
+	}
+	public void setPrice_max(int price_max) {
+		this.price_max = price_max;
+	}
+	public int getPrice_min() {
+		return price_min;
+	}
+	public void setPrice_min(int price_min) {
+		this.price_min = price_min;
+	}
+	
+	
+	
+	
+}
